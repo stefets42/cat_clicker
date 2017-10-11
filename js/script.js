c885a628702ed74aaa09a12c4cc9f89c33fb04ff
stefets42 <stefets42@users.noreply.github.com>
2017-10-11 21:41:37 +0200

    Implement counter

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+
+
+var i = 0;
+
 var count = document.getElementById("number");
-count.innerHTML = 0;
+count.innerHTML = i;
 
 $('#cat-img').click(function(){
-    var i = 0;
-	function increment(){
-	    i = i + 1;
-	};
-    count.value = i;
+	i += 1;
+	count.innerHTML = i;
 });
+
