605c524143b7628f5f780971d0c02db1f920f2e1
stefets42 <stefets42@users.noreply.github.com>
2017-10-12 15:53:25 +0200

    Implemented a second cat

--- README.md
+++ README.md
@@ -17,8 +17,11 @@
 
 ### Project instructions
 #### Visuals
-- The application should display a picture of a cat and a number of clicks.
+- The application should display two cats. Each cat includes:
+	- the cat's name
+	- a picture of the cat
+	- text showing the number of clicks
 - The specifics of the layout do not matter, so style it however you'd like.
 
 #### Interaction
-- The number of clicks should increment when the cat picture is clicked.
+- The number of clicks should increment when each cat picture is clicked.
--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
-
+// Left cat
 
 var i = 0;
 
-var count = document.getElementById("number");
-count.innerHTML = i;
+var leftCatCount = document.getElementById("left-cat-count");
+leftCatCount.innerHTML = i;
 
-$('#cat-img').click(function(){
+$('#left-cat-img').click(function(){
 	i += 1;
-	count.innerHTML = i;
+	leftCatCount.innerHTML = i;
 });
 
+
+
+var leftCatName = document.getElementById("left-cat-name");
+leftCatName.innerHTML = Maurice;
+
+
+
+
+// Right cat
+
+var j = 0;
+
+var rightCatCount = document.getElementById("right-cat-count");
+rightCatCount.innerHTML = j;
+
+$('#right-cat-img').click(function(){
+	j += 1;
+	rightCatCount.innerHTML = j;
+});
+
+
+
+var rightCatName = document.getElementById("right-cat-name");
+rightCatName.innerHTML = Roger;
